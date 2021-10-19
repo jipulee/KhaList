7949c3d15fe691c44a95bf85fe5bb471880f6965
JipuLi <jli306@sheffield.ac.uk>
2021-10-19 21:55:51 +0100

    add Layout

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Navbar from "../comps/Navbar";
+
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque fugit commodi reprehenderit, aliquid animi quos eos earum iste. Impedit consectetur laborum enim culpa molestiae! Quam veritatis quibusdam id tenetur.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque fugit commodi reprehenderit, aliquid animi quos eos earum iste. Impedit consectetur laborum enim culpa molestiae! Quam veritatis quibusdam id tenetur.</p>
+        </div>
+     );
+}
+ 
+export default About;
---
+++ pages/kha/index.js
@@ -0,0 +1,7 @@
+const Khalisting = () => {
+    return ( 
+        <div><h1>Kha Listing</h1></div>
+     );
+}
+ 
+export default Khalisting;
